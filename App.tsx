39c57e65fdf7afef6d43c64ce498fdaae1f0813c
friasrafael <rafaelfriasdacosta@gmail.com>
2021-04-21 16:46:56 -0300

    Iniciando o projeto do PlantManager
    Aplicativo desenvolvido com React Native que faz o controle dos tipos de plantas e quando elas devem ser regadas.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; //Toda estrtutura de React deve ser iniciada com a importação do React
+import { Text, View, StyleSheet } from 'react-native'; //Importando um elementos do React Native
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App() { //Aqui exportamos o App
+  return ( //Deve retornar um elemento
+    <View style={style.container} > {/*Elemento de caixa, como se fosse uma DIV. Recebem o objeto container da linha 15*/}
+      <Text>
+        Olá!
+      </Text>
+      <Text> {/* Elemento de texto */}
+      NLW#5
+      </Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
+const style = StyleSheet.create({ //Criei um objeto que recebe os estilos do programa em formato de objeto.
+  container: { /*Os elementos utilizados em style servem para centralizar o texto na página. */
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
