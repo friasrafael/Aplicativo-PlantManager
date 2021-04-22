d203f287a937cff18c2684698aeb6b5c150c5a58
friasrafael <rafaelfriasdacosta@gmail.com>
2021-04-22 02:20:54 -0300

    Finalizado o Design da tela Welcome
    Acrescentei tags para melhorar a responsividade, adicionei a fonte Jost e melhorei o design da tela primeira tela.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import React from 'react'; //Toda estrtutura de React deve ser iniciada com a importação do React
+import AppLoading from 'expo-app-loading';
+
 import { Welcome } from './src/pages/welcome'; //Importou a pa´gina "Welcome" para o app
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold 
+} from '@expo-google-fonts/jost'; //Importando a fonte Jost na raiz do APP. Foram importados alguns tipos específicos presentes na fonte Jost
 
 export default function App() { //Aqui exportamos o App
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded) //Segura a tela de Splash até o carregamento da fonte ser feito
+    return <AppLoading />
+  
+
   return (
-    <Welcome/> //Tag da página inicial
+    <Welcome /> //Tag da página inicial
   )
 }
 
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'; //Importando elementos do react-native
+import {
+    SafeAreaView,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions,
+    View
+} from 'react-native'; //Importando elementos do react-native
 /* FUNÇÕES DAS TAGS IMPORTADAS DO REACT-NATIVE:
 View = Visualização da página
 SafeAreaView = Visualização da página já levando em consideração o NavBar
@@ -10,27 +18,41 @@ StyleSheet = Estilo (Style) da página
 */
 import wateringImg from '../assets/watering.png' //importando uma imagem para a tela Welcome
 import colors from '../styles/colors'; //Importou o objeto "colors" dentro da pasta "style do projeto, que contém todas as cores utiliadas na construção do protótipo"
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts'; //Importa o doc.ts que contém as fontes que serão utilizadas no app
+import { Feather } from '@expo/vector-icons'; //Importando biblioteca de ícones do RN. Feather é uma família específica de ícones, existem várias.
 
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de{'\n'}
+                    forma fácil.
+                </Text>
 
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil.
-            </Text>
+                <Image source={wateringImg}
+                    style={styles.image}
+                    resizeMode='contain' />
 
-            <Image source={wateringImg} style={styles.image} />
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas
+                    plantas. Nós cuidamos de lembrar você
+                    sempre que precisar.
+                </Text>
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-                sempre que precisar.
-            </Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.7}
+                >
 
-            <Button title={'>'} />
-            
+                    <Feather
+                        name='chevron-right'
+                        style={styles.buttonIcon}
+                    />
+
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -38,28 +60,50 @@ export function Welcome() {
 const styles = StyleSheet.create({ //Editando o estilo da página
     container: {
         flex: 1, //Ocupa a tela inteira
+    },
+
+    wrapper: {
+        flex: 1,
         alignItems: 'center', //Texto alinhado no centro
-        justifyContent: 'space-between' //Todos os elementos distribuidos por igual
+        justifyContent: 'space-around', //Todos os elementos distribuidos por igual
+        paddingHorizontal: 20
     },
 
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 50
+        marginTop: 50,
+        lineHeight: 34,
+        fontFamily: fonts.heading
     },
 
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
 
     image: {
-        width: 292,
-        height: 284
+        height: Dimensions.get('window').width * 0.7
     },
+
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+
+    buttonIcon: {
+        color: colors.white,
+        fontSize: 32
+    }
 
 });
